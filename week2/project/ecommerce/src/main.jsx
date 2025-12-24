1a9713ae38187dee30dc72c2c937e8d9fb9ebffe
Alaa <angnm38@gmail.com>
2025-12-24 22:13:17 +0100

    Add styling and the netlify link

--- week2/project/ecommerce/src/main.jsx
+++ week2/project/ecommerce/src/main.jsx
@@ -1,25 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import React from "react";
-import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  // <Router>
-  //   <Routes>
-  //     <Route>
-
-  //     </Route>
-  //   </Routes>
-  // </Router>
-  // <StrictMode>
-  //   <App />
-  // </StrictMode>
-  <React.StrictMode>
+  <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </StrictMode>
 );
